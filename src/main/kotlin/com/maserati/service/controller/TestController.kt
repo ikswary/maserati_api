b0d2b4708776e0ef51d6bdf0e2341f41769fe4a0
SJH <Jinhyeon.son@voithru.com>
2021-03-21 18:03:02 +0900

    feat : improved testEndPoint to get TestEntity with testEntityId

--- src/main/kotlin/com/maserati/service/controller/TestController.kt
+++ src/main/kotlin/com/maserati/service/controller/TestController.kt
@@ -1,14 +1,23 @@
 package com.maserati.service.controller
 
+import com.maserati.service.service.TestService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+
 
 @Controller("")
 class TestController {
-    @GetMapping("/test")
-    fun testEndpoint(): ResponseEntity<Any> {
-        return ResponseEntity.ok()
-            .build()
+    @Autowired
+    private lateinit var testService: TestService
+
+
+    @GetMapping("/test/{testEntityId}")
+    fun testEndpoint(
+        @PathVariable testEntityId: Long
+    ): ResponseEntity<Any> {
+        return ResponseEntity.ok(testService.getTestEntityBy(testEntityId))
     }
 }
