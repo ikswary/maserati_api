414a0dc9a3770b185ac0f70c7896a8b58b075806
SJH <Jinhyeon.son@voithru.com>
2021-03-21 18:01:53 +0900

    fix : fixed WebSecurityConfig Package from service to security

--- src/main/kotlin/com/maserati/service/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/maserati/service/security/WebSecurityConfig.kt
@@ -1,4 +1,4 @@
-package com.maserati.service.service
+package com.maserati.service.security
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
