83449856fa5834cae56a7f84a1f1386a7a87e7b6
SJH <Jinhyeon.son@voithru.com>
2021-03-21 17:30:14 +0900

    feat : added WebSecurityConfig and set all opened to every path

---
+++ src/main/kotlin/com/maserati/service/service/WebSecurityConfig.kt
@@ -0,0 +1,17 @@
+package com.maserati.service.service
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@EnableWebSecurity
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/")
+            .permitAll()
+    }
+}
